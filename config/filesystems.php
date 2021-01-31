786ca371b138fc705bfc13a832b873b56afeb7e7
codedsalis <codedsalis@gmail.com>
2021-01-31 22:00:11 +0100

    Added the media local disk for saving product previews and logos

--- config/filesystems.php
+++ config/filesystems.php
@@ -42,6 +42,13 @@ return [
             'visibility' => 'public',
         ],
 
+        'media' => [
+            'driver' => 'local',
+            'root' => storage_path('app/media'),
+            'url' => env('APP_URL').'/storage',
+            'visibility' => 'public',
+        ],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
